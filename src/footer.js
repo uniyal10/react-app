e86a3f1b748f746576672b97e54980cbb430305f
sudhanshu uniyal <uniyal1001@gmail.com>
2019-10-06 20:50:27 +0530

    react-app

---
+++ src/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class FOOTER extends Component {
+render(){
+  return(
+
+          <footer class="py-5 bg-dark">
+            <div class="container">
+              <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+            </div>
+          </footer>
+
+  );
+}
+}
+export default FOOTER;
---
+++ src/sidebar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class SIDEBAR extends Component{
+  render(){
+    return(
+
+      
+    );
+  }
+}
+export default SIDEBAR;
